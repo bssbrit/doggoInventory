5cbfe8993a0099c44a07b993b01d02b27d8df68a
bssbrit <brunosabrito2@gmail.com>
2023-12-09 19:51:05 -0300

    made some progress, found an error in the sample docs i provided, they are missing atributtes

--- routes/Races.js
+++ routes/Races.js
@@ -1,13 +1,25 @@
 const express = require("express");
 var router = express.Router();
-const { Race } = require("../models/doggos");
+const { Race, Doggo } = require("../models/doggos");
 
 router.get("/", function (req, res, next) {
   Race.find()
     .then((result) => {
-      res.render("races", { races: result });
+      res.render("Races", { title: "DoggoInventory", races: result });
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Race.findById(id)
+    .then((result) => {
+      res.render("raceDetails", { race: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+module.exports = router;
